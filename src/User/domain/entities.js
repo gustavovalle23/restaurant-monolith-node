804bba744c9b447936d12919c88981788526c0e5
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-04-17 20:37:23 -0300

    :sparkles: feat(user): add a functional programming approach at object creation

--- src/User/domain/entities.js
+++ src/User/domain/entities.js
@@ -4,27 +4,55 @@ const { UserValidator } = require('./validators');
 const { Address } = require('./valueObjects');
 
 class User {
-    constructor({ name, email, password, birthDate, address: { country, state, street, number }, isActive = true, id = null }) {
-        this.id = id ?? new ObjectId().toString()
-        this.name = name
-        this.email = email
-        this.password = password
-        this.birthDate = birthDate
-        this.isActive = isActive
-        this.address = new Address({ country, state, street, number })
+  constructor({ name, email, password, birthDate, address: { country, state, street, number }, isActive = true, id = null }) {
+    this.id = id ?? new ObjectId().toString()
+    this.name = name
+    this.email = email
+    this.password = password
+    this.birthDate = birthDate
+    this.isActive = isActive
+    this.address = new Address({ country, state, street, number })
 
-        this.#validate();
-    }
+    this.#validate();
+  }
 
-    #validate() {
-        if (!ObjectId.isValid(this.id)) throw new Error('ValidationError: "id" must be a valid ObjectID');
+  #validate() {
+    if (!ObjectId.isValid(this.id)) throw new Error('ValidationError: "id" must be a valid ObjectID');
 
-        const { error } = UserValidator.validate(this, { abortEarly: false });
-        if (typeof error !== 'undefined') throw new Error(error);
-    }
+    const { error } = UserValidator.validate(this, { abortEarly: false });
+    if (typeof error !== 'undefined') throw new Error(error);
+  }
 
 }
 
+const createUser = ({ name, email, password, birthDate, address: { country, state, street, number }, isActive = true, id = null }) => {
+  const userId = id || new ObjectId().toString();
+  const address = createAddress({ country, state, street, number });
+
+  const validate = (user) => {
+    if (!ObjectId.isValid(user.id)) throw new Error('ValidationError: "id" must be a valid ObjectID');
+
+    const { error } = UserValidator.validate(user, { abortEarly: false });
+    if (typeof error !== 'undefined') throw new Error(error);
+  }
+
+  const user = {
+    id: userId,
+    name,
+    email,
+    password,
+    birthDate,
+    isActive,
+    address
+  };
+
+  validate(user);
+
+  return user;
+};
+
+
 module.exports = {
-    User
+  User,
+  createUser,
 }
--- src/User/domain/valueObjects.js
+++ src/User/domain/valueObjects.js
@@ -16,6 +16,27 @@ class Address {
     }
 }
 
+
+const createAddress = ({ country, state, street, number }) => {
+    const address = {
+        country,
+        state,
+        street,
+        number
+    };
+
+    const validate = () => {
+        const { error } = AddressValidator.validate(address, { abortEarly: false });
+        if (typeof error !== 'undefined') throw new Error(error);
+    };
+
+    validate();
+
+    return address;
+};
+
+
 module.exports = {
     Address,
+    createAddress,
 }
