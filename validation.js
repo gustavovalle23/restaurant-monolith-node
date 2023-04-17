e8f70b5ff461c9e5f0445320b9bd4940a09ddf8c
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-04-17 20:36:36 -0300

    :card_index: refactor: change to yarn and remove useless files

--- validation.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi')
-
-const schema = Joi.object({
-    username: Joi.string().required(),
-    password: Joi.string().required()
-})
-
-console.info(JSON.stringify(schema.validate({}, { abortEarly: false,  })))
