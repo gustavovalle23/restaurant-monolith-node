557dbe28d9e35c15e5bbd789793f70e0eee162d4
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-04-19 23:24:27 -0300

    :sparkles: feat(koa-entrypoint): add a logger that console the response time between middlewares

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const Koa = require('koa')
+const Koa = require('koa');
+const app = new Koa();
 
-const app = new Koa()
-
-app.use(async ctx => {
-    ctx.body = { message: 'Hello World' }
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 })
 
-console.log('Server started')
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
 
-app.listen(3000)
+app.use(async ctx => {
+  ctx.body = { 'message': 'Hello World' };
+});
+
+app.listen(3000);
